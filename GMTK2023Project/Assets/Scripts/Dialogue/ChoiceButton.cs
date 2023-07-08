96d5586dab99816e038812a296c6f1c2c5e765e2
Jake Ozer <jakobozer1@gmail.com>
2023-07-08 14:51:04 -0500

    push
    push with ui polish

--- GMTK2023Project/Assets/Scripts/Dialogue/ChoiceButton.cs
+++ GMTK2023Project/Assets/Scripts/Dialogue/ChoiceButton.cs
@@ -1,16 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
 
-public class ChoiceButton : MonoBehaviour
+public class ChoiceButton : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
     public bool isChoice1;
     public AudioClip buttonSound;
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
 
     public void ButtonClick()
     {
         SoundManager.instance.PlaySound(buttonSound);
         FindObjectOfType<ChoiceManager>().MakeChoice(isChoice1);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        anim.SetBool("grow", true);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        anim.SetBool("grow", false);
     }
 
 }
